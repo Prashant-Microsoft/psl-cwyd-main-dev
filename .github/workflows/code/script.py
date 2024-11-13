fc843c6d9e98430ae76058aeae23baf7164c8881
Prashant-Microsoft <v-pmalusare@microsoft.com>
2024-11-13 20:38:44 +0530

    testing automation flow

---
+++ .github/workflows/code/script.py
@@ -0,0 +1,9 @@
+import os
+
+# Get the secret from environment variables
+my_secret = os.getenv('MY_SECRET')
+
+if my_secret:
+    print(f"The client value is: {my_secret}")
+else:
+    print("client id not found!")
